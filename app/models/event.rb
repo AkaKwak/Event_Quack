387a9a4e3b56a1aa45e84040962227c3e576b979
AkaKwak <christophe.alaterre@gmail.com>
2024-11-05 10:59:26 +0100

    Add validation into models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,12 @@
 class Event < ApplicationRecord
   belongs_to :user
+  has_many :attendances
+  has_many :participants, through: :attendances, source: :user
+
+  validates :start_date, presence: true
+  validates :duration, presence: true, numericality: { greater_than: 0, only_integer: true, multiple_of: 5 }
+  validates :title, presence: true, length: { in: 5..140 }
+  validates :description, presence: true, length: { in: 20..1000 }
+  validates :price, presence: true, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 1000 }
+  validates :location, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_many :events, foreign_key: 'user_id', class_name: 'Event', dependent: :destroy
+  has_many :attendances
+  has_many :attended_events, through: :attendances, source: :event
+
+  validates :email, presence: true, uniqueness: true
+  validates :first_name, :last_name, presence: true
 end
