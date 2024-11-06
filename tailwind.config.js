1a07ecfbe17e7bdf047e0fc1f3c6e4080952f269
AkaKwak <christophe.alaterre@gmail.com>
2024-11-06 13:49:30 +0100

    ajout de class css par tailwind / css index events / boite de notifications alert fade in and out / tailwind.config.js / application style css

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./app/**/*.{html,js,erb}", // Assurez-vous que ce chemin correspond à votre structure de projet
+    "./app/views/**/*.{html,erb}",
+    "./app/components/**/*.{html,erb}",
+    "./app/helpers/**/*.{html,erb}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
